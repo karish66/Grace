44eec333ddd06807dcb14e4d75123bb5887657f5
Shubham Srivastava <shubham9918sri@gmail.com>
2019-03-09 11:19:22 +0530

    usre model done

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,30 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const upload = multer(multerConfig);
+const multer  = require('multer');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+
+const SignUp = require('../controllers/signup.controller');
+const Auth = require('../controllers/auth.controller');
+
+
+const router = express.Router();
+
+
+router.post('/sign-up',
+    Auth.validateSession(),
+    signUp.hashPassword(),
+    signUp.addUser());
+
+router.post('/verify/send-otp',
+    signUp.generateOPT(),
+    signUp.saveOTP(),
+    mailer.sendOTP());
+
+router.post('/verify/validate-otp', SignUp.verifyOTP());
+
+router.get('/login', Auth.isNotLoggedIn(), Auth.redirectForLogin());
+
+router.post('/login', Auth.isNotLoggedIn(), Auth.login());
+
 
 module.exports = router;
